13e7219e27ff0a05298b00b609105b8f386d1bf6
gwanghee0305 <gwanghee0305@gmail.com>
2026-09-02 14:44:46 +0900

    매핑주소 수정

--- src/main/java/com/theonefive/customer/controller/CustomerController.java
+++ src/main/java/com/theonefive/customer/controller/CustomerController.java
@@ -155,11 +155,11 @@ public class CustomerController {
     // 4.마이페이지 
     // ==========================================
     
-    @GetMapping("/mypage/index")                                 // ③ 메소드 내용만 교체
+    @GetMapping("/mypage/index")                                
     public String myPage(HttpSession session, Model model) {
         String loginId = (String) session.getAttribute("loginId");
         if(loginId == null) {
-            return "redirect:login";
+            return "redirect:/login";
         }
         CustomerDTO customer = customerService.getCustomerByLoginId(loginId);
         model.addAttribute("customer", customer);
